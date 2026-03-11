e687c6c4c267f6f833f934681fd581726153e2ea
Max Ferreira <max.alexxandre@gmail.com>
2026-03-11 18:57:44 -0300

    Inserindo mais animações no css e adicionando a lib animate.css via link cdn

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,7 +9,10 @@
 
           <div class="card-body bg-pokebola bg-normal">
             <div class="pokemon">
-              <transition name="zoom">
+              <transition
+                enter-active-class="animate__animated animate__bounceIn"
+                leave-active-class="animate__animated animate__bounceOut"
+              >
                 <img src="@/assets/imgs/pokemons/001.png" v-if="exibir">
               </transition>
 
