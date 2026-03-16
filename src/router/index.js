329a3452930784ca59b3a8ceda1805d66327253d
Max Ferreira <max.alexxandre@gmail.com>
2026-03-16 18:42:50 -0300

    Adicionando outros pokemons ajustando efeitos de seleção e implementando as views: Sobre, Habilidades e Status

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,26 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 import Home from "@/views/Home.vue";
+import Habilidades from "@/views/Habilidades.vue";
+import Sobre from "@/views/Sobre.vue";
+import Status from "@/views/Status.vue";
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: Home
-  }
+    {
+        path: '/',
+        name: 'home',
+        component: Home,
+        children: [
+            {path: 'habilidades', component: Habilidades},
+            {path: 'sobre', component: Sobre},
+            {path: 'status', component: Status}
+
+        ]
+    }
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+    history: createWebHistory(process.env.BASE_URL),
+    routes
 })
 
 export default router
